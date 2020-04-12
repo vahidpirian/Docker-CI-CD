9dfbc0bb7e686372f74bcd5bdf6c86da361ccd6e
vahid pirian <pirianit@gmail.com>
2020-04-12 19:50:16 +0430

    create build section from jenkinsfile

--- pipeline/jenkins/build/build.sh
+++ pipeline/jenkins/build/build.sh
@@ -4,7 +4,7 @@
  ## This script for build jar from maven:3-alpine image ##
  ########################################################
 
-docker run --rm -v $PWD/java:/java -v /root/.m2/:/root/.m2/ -w /java maven:3-alpine mvn "$@"
+docker run --rm -v $PWD/java:/java -v /root/.m2/:/root/.m2/ -w /java maven:3-alpine "$@"
 
 ## /java:/java => Mount the project in the image
 ## /root/.m2/:/root/.m2/ => Save all downloaded packages when Build jar
